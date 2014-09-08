79e3c03e02106cfc6f244e0303781651d5a9e43f
Roof <roof@odd-e.co.th>
2014-09-08 22:05:05 +0700

    Using standard errors package

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -1,6 +1,9 @@
 package captcha
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
 type Captcha struct {
 	leftOperand  int
@@ -8,25 +11,17 @@ type Captcha struct {
 	rightOperand int
 }
 
-type errorString struct {
-	s string
-}
-
-func (e *errorString) Error() string {
-	return e.s
-}
-
 func New(leftOperand int, operator int, rightOperand int) (*Captcha, error) {
 	c := Captcha{}
 	if leftOperand > 9 {
-		return nil, &errorString{"Left operand value could not greater than 9"}
+		return nil, errors.New("Left operand value could not greater than 9")
 	} else if leftOperand < 0 {
-		return nil, &errorString{"Left operand value could not less than 0"}
+		return nil, errors.New("Left operand value could not less than 0")
 	}
 	c.leftOperand = leftOperand
 	c.rightOperand = rightOperand
 	c.operator = operator
-	return &c, &errorString{""}
+	return &c, nil
 }
 
 func (c *Captcha) LeftOperand() string {
